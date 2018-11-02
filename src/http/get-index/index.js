6947e0e9ba104e0f6f6e7e6ad8d961303f672781
Mike MacCana <mike@certsimple.com>
2018-11-02 17:58:16 +0000

    Use arc4 style await function for / Use requireLogin and protected rather than 'protect' and 'attack' because not everyone knows memes Add email and password to instructions Make email 'admin@example.com' (an actual email) rather than 'admin' Prettify

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,46 +1,41 @@
-let arc = require('@architect/functions')
+let arc = require("@architect/functions")
 let url = arc.http.helpers.url
 
-function index(req, res) {
-  var header = `<h1>Login Demo</h1>`
-  var protec = `<a href=${url('/protected')}>protected</a>`
-  var logout = `<a href=${url('/logout')}>logout</a>`
-  var nav = `<p>${protec} | ${logout}</p>`
+// Callback style works too
+// function index(req, res) {
+//   var header = `<h1>Login Demo</h1>`
+//   var protec = `<a href=${url('/protected')}>protected</a>`
+//   var logout = `<a href=${url('/logout')}>logout</a>`
+//   var nav = `<p>${protec} | ${logout}</p>`
 
-  var form = `
-    <form action=${url('/login')} method=post>
-      <label for=email>Email</label>
-      <input type=text name=email>
-      <label for=password>Password</label>
-      <input type=password name=password>
-      <button>Login</button>
-    </form>
-  `
+//   var form = `
+//     <form action=${url('/login')} method=post>
+//       <label for=email>Email</label>
+//       <input type=text name=email>
+//       <label for=password>Password</label>
+//       <input type=password name=password>
+//       <button>Login</button>
+//     </form>
+//   `
 
-  res({
-    html: `${header} ${req.session.isLoggedIn? nav : form}`
-  })
-}
+//   res({
+//     html: `${header} ${req.session.isLoggedIn? nav : form}`
+//   })
+// }
 
-exports.handler = arc.http(index)
-
-/*
- * of course the async/await style works too
- *
-let arc = require('@architect/functions')
-let url = arc.http.helpers.url
+// exports.handler = arc.http(index)
 
 exports.handler = async function http(req) {
-  let state = await arc.http.session.read(req)
-  let isLoggedIn = !!state.isLoggedIn
+	let state = await arc.http.session.read(req)
+	let isLoggedIn = !!state.isLoggedIn
 
-  var header = `<h1>Login Demo</h1>`
-  var protec = `<a href=${url('/protected')}>protected</a>`
-  var logout = `<a href=${url('/logout')}>logout</a>`
-  var nav = `<p>${protec} | ${logout}</p>`
+	var header = `<h1>Login Demo</h1>`
+	var protec = `<a href=${url("/protected")}>protected</a>`
+	var logout = `<a href=${url("/logout")}>logout</a>`
+	var nav = `<p>${protec} | ${logout}</p>`
 
-  var form = `
-    <form action=${url('/login')} method=post>
+	var form = `
+    <form action=${url("/login")} method=post>
       <label for=email>Email</label>
       <input type=text name=email>
       <label for=password>Password</label>
@@ -49,9 +44,8 @@ exports.handler = async function http(req) {
     </form>
   `
 
-  return {
-    type: 'text/html',
-    body: `${header} ${isLoggedIn? nav : form}`
-  }
+	return {
+		type: "text/html",
+		body: `${header} ${isLoggedIn ? nav : form}`
+	}
 }
-*/
--- src/http/get-protected/index.js
+++ src/http/get-protected/index.js
@@ -1,23 +1,23 @@
-let arc = require('@architect/functions')
+let arc = require("@architect/functions")
 let url = arc.http.helpers.url
+const log = console.log.bind(console)
 
-function protect(req, res, next) {
-  if (req.session.isLoggedIn) {
-    next()
-  }
-  else {
-    res({
-      location: url(`/`)
-    })
-  }
+function requireLogin(req, res, next) {
+	if (req.session.isLoggedIn) {
+		next()
+	} else {
+		res({
+			location: url(`/`)
+		})
+	}
 }
 
-function attack(req, res) {
-  let msg = 'oh hai you must be logged in to see me!'
-  let logout = `<a href=${url('/logout')}>logout</a>`
-  res({
-    html: `${msg} ${logout}`
-  })
+function protected(req, res) {
+	let msg = "oh hai you must be logged in to see me!"
+	let logout = `<a href=${url("/logout")}>logout</a>`
+	res({
+		html: `${msg} ${logout}`
+	})
 }
 
-exports.handler = arc.http(protect, attack)
+exports.handler = arc.http(requireLogin, protected)
